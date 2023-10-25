9e863e84b2b768c7d9e5d9b3909a1baddd79316f
DGrazz <mariogalo2008@gmail.com>
2023-10-25 16:46:02 +0200

    pantalla de compraFeta

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -23,19 +23,19 @@ createApp({
             this.cartPrice += this.products[index].price;
             console.log(this.cartPrice);
 
-            // let objetoExistente = this.cart.find(item =>item.id === this.productes[index].id)
-            // if (objetoExistente) {
-            //     this.cartPrice += this.products[index].price;
-            //     this.cart.find(item => item.id )
-            // } else {
-            //   this.cart[this.comprat.length] = {
-            //     id: this.products[index].id,
-            //     Title: this.products[index].Title,
-            //     preu: this.products[index].preu,
-            //     Quantitat: 1,
-            //    };
-            //    this.totalCompra+=this.productes[index].preu;
-            // }
+            let objetoExistente = this.cart.find(item =>item.id === this.productes[index].id)
+            if (objetoExistente) {
+                this.cartPrice += this.products[index].price;
+                this.productes[index].amount++;
+            } else {
+              this.cart[this.comprat.length] = {
+                id: this.products[index].id,
+                Title: this.products[index].Title,
+                preu: this.products[index].preu,
+                amount: 1,
+               };
+               this.totalCompra+=this.productes[index].preu;
+            }
         }
     },
     created() {
