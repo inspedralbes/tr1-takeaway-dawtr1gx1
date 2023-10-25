f13dca7458cc54f2f7493f12c04a3311ae2abfb0
a16margarlop <a16margarlop@inspedralbes.cat>
2023-10-25 11:01:25 +0200

    millorant ticket, y fent pagina de dadesCompra

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -16,12 +16,26 @@ createApp({
             this.active = active;
 
         },
-        addToCart(id) {
-            this.cart.push(this.products[id]);
+        addToCart(index) {
+            this.cart.push(this.products[index]);
             console.log(this.cart);
 
-            this.cartPrice += this.products[id].price;
+            this.cartPrice += this.products[index].price;
             console.log(this.cartPrice);
+
+            // let objetoExistente = this.cart.find(item =>item.id === this.productes[index].id)
+            // if (objetoExistente) {
+            //     this.cartPrice += this.products[index].price;
+            //     this.cart.find(item => item.id )
+            // } else {
+            //   this.cart[this.comprat.length] = {
+            //     id: this.products[index].id,
+            //     Title: this.products[index].Title,
+            //     preu: this.products[index].preu,
+            //     Quantitat: 1,
+            //    };
+            //    this.totalCompra+=this.productes[index].preu;
+            // }
         }
     },
     created() {
