f17cb059a8d43047dee9d7a634cc6fca1bc4cafd
a16margarlop <a16margarlop@inspedralbes.cat>
2023-10-30 13:03:24 +0100

    .

--- laravel/apiProductes/app/Http/Controllers/orderController.php
+++ laravel/apiProductes/app/Http/Controllers/orderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class orderController extends Controller
 {
@@ -29,12 +30,25 @@ class orderController extends Controller
      */
     public function store(Request $request)
     {
-        $newOrder= new order;
-        $newOrder->jsonOrder=$request->jsonOrder;
-        $newOrder->totalPrice=$request->totalPrice;
-        $newOrder->mail=$request->mail;
-        $newOrder->save();
-        return $newOrder;
+        $validator = Validator::make($request->all(), [
+            'jsonOrder' => 'required',
+            'totalPrice' => 'required',
+            'mail' => 'required|email',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => 'El correo electrónico no es válido.'], 422);
+        }else {
+            $newOrder = new Order;
+            $newOrder->jsonOrder = $request->jsonOrder;
+            $newOrder->totalPrice = $request->totalPrice;
+            $newOrder->mail = $request->mail;
+            $newOrder->save();
+
+            return $newOrder;
+        }
+
+        
     }
 
     /**
