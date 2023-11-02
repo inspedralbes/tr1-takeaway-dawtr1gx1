32855b3d4b084adabcd9a0530ca11b6bdde11945
a16margarlop <a16margarlop@inspedralbes.cat>
2023-11-02 12:27:23 +0100

    qr perfecte

--- laravel/apiProductes/resources/views/pdf.blade.php
+++ laravel/apiProductes/resources/views/pdf.blade.php
@@ -8,8 +8,43 @@
 
     <style>
         .container{
-            width: 500px;
-            
+            width: 100%;
+            height: 100%;
+            min-height: 600px;
+        }
+
+        table{
+            width: 100%;
+            text-align: center;
+            border-radius: 10px;
+            border: 1px solid black;
+            border-collapse: collapse;
+            margin: 0 auto;
+        }
+        .title th{
+            padding-bottom: 15px;
+        }
+        .ticket tr{
+            border: 1px solid black;
+        }
+        .ticket th{
+            padding: 10px 20px;
+            border: 1px solid black;
+        }
+        .ticket td{
+            padding: 5px 10px;
+            border: 1px solid black;
+        }
+        .productName{
+            width: 60%;
+            text-align: left;
+        }
+        .bottom-line td{
+            width: 50%;
+        }
+        .qr{
+            text-align:center;
+            margin-top: 40px;
         }
     </style>
 
@@ -18,24 +53,42 @@
     
     <div class="container">
         <table>
+
+            <tr class="title">
+                <th colspan="2">
+                INFORMACIO DE LA COMANDA
+                </th>
+            </tr>
+            <tr class="bottom-line">
+                <td>Numero de la comanda: 19</td>
+                <td>Email: a16margarlop@inspedralbes.cat</td>
+            </tr>
+            <tr class="bottom-line">
+                <td>Data de la comanda: 02-11-2023</td>
+            </tr>
+        </table>
+        
+        <table class="ticket">
             <tr>
                 <th>Producte</th>
                 <th>Quantitat</th>
                 <th>Preu</th>
             </tr>
             <tr>
-                <td>Hamburguesa</td>
+                <td class="productName">Hamburguesa</td>
                 <td>2</td>
                 <td>7.99</td>
             </tr>
             <tr>
-                <td>Arros Tres Delicies</td>
+                <td class="productName">Arros Tres Delicies</td>
                 <td>1</td>
                 <td>9.99</td>
             </tr>
         </table>
 
-        {!! QrCode::generate('hola'); !!}
+        <div class="qr">
+            {!! QrCode::generate('Mail, ID, Ticket'); !!}
+        </div>
     </div>
 
 </body>
