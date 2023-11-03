54b152ccf489310b5c5cb22cec5b294133c3a37a
EGVPedralbes <a20erigomvil@inspedralbes.cat>
2023-11-03 08:32:57 +0100

    NameAdded

--- laravel/apiProductes/resources/views/adminStock.blade.php
+++ laravel/apiProductes/resources/views/adminStock.blade.php
@@ -9,7 +9,7 @@
     <form action="{{route('updateItem', ['id' => $item->id])}}" method="post">
     @csrf
     @method('PATCH')
-    <li>Item id:{{$item->id}}> <input type="number" value="{{$item->stock}}" name="stock"> <input type="submit" value="Guardar Stock" ></li>
+    <li>Item id:{{$item->id}}, {{$item->itemName}}> <input type="number" value="{{$item->stock}}" name="stock"> <input type="submit" value="Guardar Stock" ></li>
 
     </form>
     @endforeach
