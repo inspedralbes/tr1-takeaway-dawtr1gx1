0b2367f65c2b967d0ec59a8d02bf094991ba2b53
a16margarlop <a16margarlop@inspedralbes.cat>
2023-11-09 13:05:37 +0100

    editar producte fet

--- laravel/apiProductes/resources/views/adminStock.blade.php
+++ laravel/apiProductes/resources/views/adminStock.blade.php
@@ -9,7 +9,17 @@
     <form action="{{route('updateItem', ['id' => $item->id])}}" method="post">
     @csrf
     @method('PATCH')
-    <li>Item id:{{$item->id}}, {{$item->itemName}}> <input type="number" value="{{$item->stock}}" name="stock"> <input type="submit" value="Guardar Stock" ></li>
+    <li>Item id:{{$item->id}}, {{$item->itemName}}> 
+        <br><label for="">Name:</label>
+        <input type="text" value="{{$item->itemName}}" name="itemName">
+        <label for="">Description:</label>
+        <input type="text" value="{{$item->description}}" name="description">
+        <label for="">Price:</label>
+        <input type="number" value="{{$item->price}}" name="price">
+        <label for="">Stock:</label>
+        <input type="number" value="{{$item->stock}}" name="stock">
+        <input type="submit" value="Guardar" >
+    </li>
 
     </form>
     @endforeach
