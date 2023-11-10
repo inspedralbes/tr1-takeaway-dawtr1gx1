cd5c011191ebf9b72ac4c7bfbbde5f384e886e03
a16margarlop <a16margarlop@inspedralbes.cat>
2023-11-10 10:40:59 +0100

    canvi text button

--- laravel/apiProductes/resources/views/adminItems.blade.php
+++ laravel/apiProductes/resources/views/adminItems.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <h2>Administracio de items</h2>
 <ul>
-    <a href="{{route('adminStock')}}" class="btn btn-primary">Gestionar stock</a>
+    <a href="{{route('adminStock')}}" class="btn btn-primary">Modificar productes</a>
     <a href="{{route('afegirItem')}}" class="btn btn-primary">Afegir item</a>
     <a href="{{route('landing')}}" class="btn btn-primary">Tornar</a>
 </ul>
